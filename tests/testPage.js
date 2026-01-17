7a0659931249919ff78b660a079fe7fb5900a525
antareepsarkar <sarkar.antareep.9035@gmail.com>
2026-01-17 12:45:18 +0530

    CHORE: fix linter issues

--- tests/testPage.js
+++ tests/testPage.js
@@ -1,4 +1,4 @@
-import request from "supertest"; 
+import request from "supertest";
 
 import {readDoc} from "../src/lib/utils.js";
 import run from "../src/run.js";
@@ -15,21 +15,21 @@ run(args);
 const app = Server.app;
 
 request(app)
-	.get("/tests/index.html")
-	.end(check);
+    .get("/tests/index.html")
+    .end(check);
 
 function check(err, res){
-	if(err){
-		throw err;
-	}
-	let data = readDoc("./tests/index.html");
-	data = data + readDoc("./src/assets/socket.html");
-	if(data != res.text){
-		throw new Error("Received text different from expected text");
-	}
-	log("Test for checking webpage loading passed!");
-	if(Server.listener){
-		Server.close();
-	}
-	process.exit(0);
-}	
+    if(err){
+        throw err;
+    }
+    let data = readDoc("./tests/index.html");
+    data = data + readDoc("./src/assets/socket.html");
+    if(data != res.text){
+        throw new Error("Received text different from expected text");
+    }
+    log("Test for checking webpage loading passed!");
+    if(Server.listener){
+        Server.close();
+    }
+    process.exit(0);
+}
